b4b5670bb3c423743cedc72740245415a80bb9e4
cko <cornelius.koller@online.de>
2025-07-21 12:07:50 +0200

    move pgvector to testContainers

--- src/test/java/de/koware/cfs/chatwith10k/rag/VectorStoreTest.java
+++ src/test/java/de/koware/cfs/chatwith10k/rag/VectorStoreTest.java
@@ -1,11 +1,17 @@
 package de.koware.cfs.chatwith10k.rag;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 import java.util.Map;
@@ -13,11 +19,32 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Testcontainers
 public class VectorStoreTest {
 
 
     @Autowired
     VectorStore vectorStore;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("pgvector/pgvector:pg17");
+
+    @BeforeAll
+    static void setup() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void teardown() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
 
     @Test
     public void vectorStoreTest_basic() {
